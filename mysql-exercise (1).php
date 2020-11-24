1bfb5d281244d911fedcca688a8d63f4b7a6f2de
Sadi-Ashlaq <71584001+Sadi-Ashlaq@users.noreply.github.com>
2020-11-24 22:36:07 +0200

    Done

---
+++ mysql-exercise (1).php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.0.3
+ */
+
+/**
+ * Database `mysql-exercise`
+ */
+
+/* `mysql-exercise`.`countries` */
+$countries = array(
+);
+
+/* `mysql-exercise`.`dup_countries` */
+$dup_countries = array(
+);
+
+/* `mysql-exercise`.`jobs` */
+$jobs = array(
+);
+
+/* `mysql-exercise`.`job_history` */
+$job_history = array(
+);
